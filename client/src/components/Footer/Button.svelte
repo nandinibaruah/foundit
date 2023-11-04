09ec0ebf76e86f5b49515e018a65b203a8a9f316
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 15:12:27 -0700

    buttons

--- client/src/components/Footer/Button.svelte
+++ client/src/components/Footer/Button.svelte
@@ -1,3 +1,9 @@
-<button class="w-32 bg-slate-100 hover:bg-slate-200 p-2 rounded-md">
-  <slot/>
-</button>
+<script>
+  export let href;
+</script>
+
+<a href={href} class="w-32 bg-slate-100 hover:bg-slate-200 hover:cursor-pointer p-2 rounded-md flex h-full align-center justify-center">
+  <div class="flex mx-auto align-center justify-center h-full">
+    <slot/>
+  </div>
+</a>
