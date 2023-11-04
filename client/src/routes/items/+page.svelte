37591fd1463aa61ae7614c1eaec49ffcfd663f22
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 11:42:23 -0700

    init fe and add basic routes structure

---
+++ client/src/routes/items/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Item from "../../components/Item.svelte";
+  const items = ["item 1", "item 2", "item 3"];
+</script>
+
+<h1>Items List</h1>
+<div>
+{#each items as item, index}
+<Item id={index}/>
+{/each}
+</div>
