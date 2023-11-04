93c3613549932d4f8ff252b8f9eb4d23ee29e0cf
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 15:53:21 -0700

    styles

--- client/src/components/Footer/Footer.svelte
+++ client/src/components/Footer/Footer.svelte
@@ -2,7 +2,7 @@
   import Icon from "$components/Icon.svelte";
 import Button from "./Button.svelte";
 </script>
-<div class="p-5 flex justify-between">
+<div class="p-5 flex justify-between shadow-card">
   <Button>
     <Icon name="home"/>
   </Button>
