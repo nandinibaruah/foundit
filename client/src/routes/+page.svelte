765933bc0d3e2470617c0a76c8c52fd30eb9b04d
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 14:04:16 -0700

    add tailwind, create components and item detail page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+  <a href="/items">Items</a>
+  <a href="/item/123">Item Details</a>
+</p>
