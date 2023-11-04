de221a94dfb53a0adfa3795e1ae86efecb377c84
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 16:13:55 -0700

    Update +page.svelte

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,16 +2,3 @@
   import Items from "../components/Items/Items.svelte";
 </script>
 <Items/>
-
-<style>
-  .title{
-    font-family:'Times New Roman', Times, serif;
-    font-size: 5em;
-  }
-
-  .buttons{
-    font-family: 'Times New Roman', serif;
-    font-size: 3em;
-
-  }
-</style>
