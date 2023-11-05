80b1cfe96330dccd7de31d7f7a5ac197e4984667
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 18:23:29 -0700

    selected footer item

--- client/src/components/Footer/Button.svelte
+++ client/src/components/Footer/Button.svelte
@@ -1,8 +1,12 @@
 <script>
+
+  
   export let href;
+  export let selected = false;
+  export let onClick = undefined;
 </script>
 
-<a href={href} class="w-32 bg-slate-100 hover:bg-slate-200 hover:cursor-pointer p-2 rounded-md flex h-full align-center justify-center">
+<a on:click={onClick} href={href} class="w-32 {selected && "bg-slate-100"} hover:bg-slate-200 hover:cursor-pointer p-2 rounded-md flex h-full align-center justify-center">
   <div class="flex mx-auto align-center justify-center h-full">
     <slot/>
   </div>
