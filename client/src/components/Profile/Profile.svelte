a139c718c2085179a69be356136fba374ca5c5b1
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 21:21:49 -0700

    Update Profile.svelte

--- client/src/components/Profile/Profile.svelte
+++ client/src/components/Profile/Profile.svelte
@@ -1,6 +1,23 @@
 <script>
     import { supabase } from '../../supabase/supabaseClient'
-
+    import ListItem from "$components/ListItem/ListItem.svelte";
+    
+    //ITEM DETAILS
+    const items = [
+        { 
+            status: "lost", 
+            title: 'Whatever', 
+            imageUrl: 'https://images.pexels.com/photos/16776919/pexels-photo-16776919/free-photo-of-blue-motor-scooter-standing-outside-a-beauty-center.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 
+            description: 'Lost something silly.' 
+        },
+        { 
+            status: "found", 
+            title: 'ID Card', 
+            imageUrl: 'https://images.pexels.com/photos/18844140/pexels-photo-18844140/free-photo-of-building-of-natwest-bank-in-llandudno.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2', 
+            description: 'Found an identity. Dm me for it back.' 
+        },
+    ];
+    
     let session;
 
     async function getSession(){
@@ -28,6 +45,66 @@
 
     getSession()
 </script>
+
+  
+  <style>
+    .title {
+        font-size: 3em;
+        font-weight: bolder;
+        color: #4000FF;
+        text-align: left;
+        margin-bottom: 20px;
+        margin-left: 20px;
+        margin-top: 20px;
+    }
+  
+    .myItems {
+        font-size: 1.5em;
+        font-weight: bold;
+        color: #4D485B;
+        text-align: left;
+        margin-bottom: 10px;
+        margin-left: 20px;
+    }
+  
+    .signout-button {
+        color: #7556D1;
+        background-color: transparent;
+        border: 2px solid #7556D1;
+        border-radius: 5px;
+        padding: 10px 20px;
+        font-size: 1em;
+        font-weight: bold;
+        cursor: pointer;
+        margin-top: 70px;
+        margin-bottom: 10px;
+        margin-left: auto;
+        margin-right: auto;
+        display: block;
+    }
+  
+    .signout-button:hover {
+        background-color: #7556D1;
+        color: white;
+    }
+  </style>
+  
+  <h1 class="title">UserEmail</h1>
+  <p class="myItems">My Items</p>
+  
+  {#each items as item}
+    <div class="p-3 flex-col space-y-4 flex-1">
+        <ListItem
+            status={item.status}
+            title={item.title}
+            imageUrl={item.imageUrl}
+            description={item.description}
+        />
+    </div>
+  {/each}
+  
+  <button class="signout-button">Sign Out</button>
+
 
 <h1>Name: {session?.user.user_metadata.name}</h1>
 <h1>Email: {session?.user.user_metadata.email}</h1>
