5ca84c0173b3b25b2d2bf0a3d02d071c782ee30e
Noelle Szombathy <business.noellered@gmail.com>
2023-11-04 19:39:03 -0700

    server requests working

--- server/server.js
+++ server/server.js
@@ -1,44 +1,49 @@
-const express = require('express')
-const app = express()
-const supabase = require('./supabaseClient')
+const express = require("express");
+const app = express();
+const supabase = require("./supabaseClient");
+const cors = require("cors");
 
-const port = 3000
+const port = 3000;
 
-app.use(express.static('public')) // to serve images, CSS files, and JavaScript files in a directory named public
+app.use(express.static("public")); // to serve images, CSS files, and JavaScript files in a directory named public
 // http://localhost:3000/images/cat.png
 
+app.use(cors());
+
 // routes
-const items = require('./routes/get/items')
-const listItems = require('./routes/get/listItems')
-const usersItems = require('./routes/get/userItems') // Won't let me use /usersItems
+const items = require("./routes/get/items");
+const listItems = require("./routes/get/listItems");
+const usersItems = require("./routes/get/userItems"); // Won't let me use /usersItems
 
-const insertItems = require('./routes/post/insertItems')
-const updateItems = require('./routes/post/updateItems')
+const insertItems = require("./routes/post/insertItems");
+const updateItems = require("./routes/post/updateItems");
 
-const deleteItems = require('./routes/delete/deleteItems')
+const deleteItems = require("./routes/delete/deleteItems");
 //
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send({
+    message: "hello world",
+  });
+});
 
-app.get('/cat', (req, res) => {
-    res.redirect('/images/cat.png')
-})
+app.get("/cat", (req, res) => {
+  res.redirect("/images/cat.png");
+});
 
 // Each route can have one or more handler functions, which are executed when the route is matched.
 // app.METHOD(PATH, HANDLER)
 
 // SELECT: JWT Token Optional
-app.get('/items', items) // return Item[]
-app.get('/user/items', usersItems) // return Item[] with userId/jwt param
-app.get('/list/items', listItems) // return Item with itemId param
+app.get("/items", items); // return Item[]
+app.get("/user/items", usersItems); // return Item[] with userId/jwt param
+app.get("/list/items", listItems); // return Item with itemId param
 
 // requires auth: INSERT, UPDATE, DELETE with JWT Token & ItemId
-app.post('/items', insertItems)
-app.post('/items', updateItems)
-app.delete('/items', deleteItems)
+app.post("/items", insertItems);
+app.post("/items", updateItems);
+app.delete("/items", deleteItems);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
